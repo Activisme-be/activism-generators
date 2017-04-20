27323bcfcfc638ca61cb8b506c4fcece5eb726da
Tjoosten <Tjoosten3@gmail.com>
2017-04-20 18:44:07 +0200

    Add test

--- tests/CreateModelTest.php
+++ tests/CreateModelTest.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
 
 namespace ActivismeBe\Artillery\Tests;
 
 use ActivismeBe\Artillery\Commands\ModelCodeigniterCommand;
+use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class CreateModelTest extends \PHPUnit\Framework\TestCase
 {
 	public function testExecute()
 	{
-		dsd
+		$application = new Application();
+		$application->add(new ModelCodeigniterCommand);
+
+		$command = $application->find('make:model-CodeIgniter');
+		$commandTester = new CommandTester($command);
+		$commandTester->execute([
+			'command' => $command->getName()
+		]);
 	}
 }
